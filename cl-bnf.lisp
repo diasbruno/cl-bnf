c66911f98a2260b854b11cd9371e17d190091aeb
Bruno Dias <dias.h.bruno@gmail.com>
2018-05-08 02:22:29 -0300

    feature: and and or.

--- cl-bnf.lisp
+++ cl-bnf.lisp
@@ -6,7 +6,7 @@
 
 (defmacro one (stream pred)
   `(when (funcall ,pred (peek-char nil ,stream nil))
-       (read-char ,stream nil nil)))
+       (string (read-char ,stream nil nil))))
 
 (defmacro many (stream pred)
   "Read from STREAM until PRED terminates the reading."
@@ -17,26 +17,44 @@
 
 (defvar *expressions* nil)
 
-; (declaim (inline numberic-char-p))
 (defun numeric-char-p (char)
   (and (char-not-lessp char #\0)
        (char-not-greaterp char #\9)))
 
+(defun or-match (source expr)
+  (let* ((c (car expr))
+         (result (funcall c source)))
+    (if (> (length result) 0)
+        result
+        (or-match source (cdr expr)))))
+
+(defun and-match (source expr stack)
+  (if (null expr)
+      stack
+      (let* ((c (car expr))
+             (result (funcall c source)))
+        (if (> (length result) 0)
+            (and-match source (cdr expr) (push result stack))
+            nil))))
+
 (defmacro := (label rule)
   (let ((kind (car rule))
+        (rules (cdr rule))
         (expr (cadr rule)))
     `(defun ,label (source)
-       (cond
-         ((equal ,kind ':many) (many source ,expr))
-         ((equal ,kind ':one) (one source ,expr))))))
+       ,(cond
+          ((equal kind ':many) `(many source ,expr))
+          ((equal kind ':one) `(one source ,expr))
+          ((equal kind ':or) `(or-match source (list ,@rules)))
+          ((equal kind ':and) `(and-match source (list ,@rules) NIL))))))
 
-(:= identifier
-    (:many #'alpha-char-p))
-(:= numberic
-    (:many #'numeric-char-p))
+(:= identifier (:one #'alpha-char-p))
+(:= numeric (:many #'numeric-char-p))
+(:= ident-or-num (:or #'identifier #'numeric))
+(:= an (:and #'identifier #'numeric))
 
-(defun parse (source)
-  (format nil "~a"
-            (identifier source)))
+(defun parse (source rules)
+  (funcall rules source))
 
-(let ((text (string-to-stream "aa"))) (parse text))
+(let ((text (string-to-stream "a4")))
+  (parse text #'an))
